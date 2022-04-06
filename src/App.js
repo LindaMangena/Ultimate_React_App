88a19747632ff25ab17e140629083e54bf78701c
Linda <linda@bantusoft.co.za>
2022-04-07 00:09:33 +0200

    Starting React Project

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+const Person = () => {
+
+  return (
+    <>
+      <h1>Name: John </h1>
+      <h2>Last Name: Doe</h2>
+      <h2>Age: 30</h2>
+
+
+    </>
+
+  )
+
+}
+const App = () => {
+
+  return (
+    <div className="App">
+
+      <Person />
+    
+
+
+    </div>
+  );
+}
+
+export default App;
