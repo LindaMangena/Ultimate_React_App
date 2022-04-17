22051101d37dfd32ebd118012540c10994fe40b3
Linda <linda@bantusoft.co.za>
2022-04-17 22:53:00 +0200

    Getting data from API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { useEffect } from "react";
 
-const App = () =>{
-    return(
+//9e76c521
+
+const API_URL = ' http://www.omdbapi.com/?i=tt3896198&apikey=9e76c521';
+const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+
+    }
+
+    useEffect(() => {
+        searchMovies('Spiderman');
+        
+
+    }, []);
+
+    return (
         <h1>App</h1>
     );
 }
